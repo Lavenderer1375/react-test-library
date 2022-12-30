ed506c1a4aef86d093b1cae6789b4955070b466c
Lavenderer1375 <abedikianush@gmail.com>
2022-12-30 15:38:23 +0000

    Practice

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -1,110 +1,92 @@
-import { render, screen } from "@testing-library/react"
-import TodoFooter from "../TodoFooter"
-import { BrowserRouter } from "react-router-dom";
+import { render, screen } from '@testing-library/react';
+import TodoFooter from '../TodoFooter';
+import { BrowserRouter } from 'react-router-dom';
 
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
 
-const MockTodoFooter = ({numberOfIncompleteTasks}) => {
-    return (
-        <BrowserRouter>
-            <TodoFooter
-            numberOfIncompleteTasks={numberOfIncompleteTasks}
-            />
-        </BrowserRouter>
-    )
-}
+describe('test the text fot todoFooter ', () => {
+  describe('', () => {
+    it('should write task for 1 task ', () => {
+      render(<MockTodoFooter numberOfIncompleteTasks={1} />);
 
-describe("test the text fot todoFooter ", () => {
+      const onetask = screen.getByText(/1 task left/i);
+      expect(onetask).toBeInTheDocument();
+    });
+  });
 
-    describe("", () => { 
-        it("should write task for 1 task ", () => {
-        render(
-            <MockTodoFooter
-            numberOfIncompleteTasks={1}
-            />
-        )
-    
-        const onetask = screen.getByText(/1 task left/i)
-        expect(onetask).toBeInTheDocument()
-    })
-    })
+//   test('should render correct amount of tasks', () => {
+//     render(<MockTodoFooter numberOfIncompleteTasks={3} />);
+//     const pElement = screen.getByText(/3 tasks left/i);
+//     expect(pElement).toBeInTheDocument();
+//   });
+});
 
-test("should render correct amount of tasks", () => {
-    render(
-        <MockTodoFooter
-            numberOfIncompleteTasks={3}
-        />
-    );
-    const pElement = screen.getByText(/3 tasks left/i);
-    expect(pElement).toBeInTheDocument();
-})
-})
+// //!toBeTruthy
+// it('should write task for 1 task ', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
 
-//!toBeTruthy
-   it("should write task for 1 task ", async() => {
-        render(
-            <MockTodoFooter
-            numberOfIncompleteTasks={1}
-            />
-        )
-    
-       const onetask = await screen.queryByText(/1 task left/i)
-    //    const onetask = await screen.queryByText(/1 task left/i)
-       // falsy=>false nan undefined null ""
-        // expect(onetask).not.toBeTruthy()
-        expect(onetask).toBeTruthy()
-    })
+//   const onetask = await screen.queryByText(/1 task left/i);
+//   //    const onetask = await screen.queryByText(/1 task left/i)
+//   // falsy=>false nan undefined null ""
+//   // expect(onetask).not.toBeTruthy()
+//   expect(onetask).toBeTruthy();
+// });
 
-//!ToBeVisible()
+// //!ToBeVisible()
 
-    it("should write task for 1 task ", () => {
-        render(
-            <MockTodoFooter
-            numberOfIncompleteTasks={1}
-            />
-        )
-    
-        const onetask = screen.getByText(/1 task left/i)
-        expect(onetask).toBeVisible()
-    })
+//     it("should write task for 1 task ", () => {
+//         render(
+//             <MockTodoFooter
+//             numberOfIncompleteTasks={1}
+//             />
+//         )
 
-    //! toContainHTML()
-        it("should write task for 1 task ",async () => {
-        render(
-            <MockTodoFooter
-            numberOfIncompleteTasks={1}
-            />
-        )
-    
-            const onetask = await screen.queryByText(/1 task left/i)
-            console.log(onetask);
-        expect(onetask).toContainHTML("p")
-        })
-    
-        //! toHaveTextContent()
-   it("should write task for 1 task ",async () => {
-        render(
-            <MockTodoFooter
-            numberOfIncompleteTasks={1}
-            />
-        )
-    
-            const onetask = await screen.queryByText(/1 task left/i)
-            console.log(onetask);
-        expect(onetask).toHaveTextContent("1 task left")
-   })
-        
-   //!ToBe()
+//         const onetask = screen.getByText(/1 task left/i)
+//         expect(onetask).toBeVisible()
+//     })
 
-     it("should write task for 1 task ",async () => {
-        render(
-            <MockTodoFooter
-            numberOfIncompleteTasks={1}
-            />
-        )
-    
-            const onetask = await screen.queryByText(/1 task left/i)
-            
-        expect(onetask.textContent).toBe("1 task left")
-   })
+//     //! toContainHTML()
+//         it("should write task for 1 task ",async () => {
+//         render(
+//             <MockTodoFooter
+//             numberOfIncompleteTasks={1}
+//             />
+//         )
 
- 
+//             const onetask = await screen.queryByText(/1 task left/i)
+//             console.log(onetask);
+//         expect(onetask).toContainHTML("p")
+//         })
+
+//         //! toHaveTextContent()
+//    it("should write task for 1 task ",async () => {
+//         render(
+//             <MockTodoFooter
+//             numberOfIncompleteTasks={1}
+//             />
+//         )
+
+//             const onetask = await screen.queryByText(/1 task left/i)
+//             console.log(onetask);
+//         expect(onetask).toHaveTextContent("1 task left")
+//    })
+
+//    //!ToBe()
+
+//      it("should write task for 1 task ",async () => {
+//         render(
+//             <MockTodoFooter
+//             numberOfIncompleteTasks={1}
+//             />
+//         )
+
+//             const onetask = await screen.queryByText(/1 task left/i)
+
+//         expect(onetask.textContent).toBe("1 task left")
+//    })
